d3047e92cc1573f587c599679a76c974a4f88362
atembamanu <atembaemm07@gmail.com>
2019-08-14 14:22:19 +0300

    create empty functions

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,30 @@ var akanArray = [
         Saturday:"Ama"
     }
 ]
+
+//get user date input
+function getUserBirthday(date){
+
+}
+
+//verify the date the user entered
+function verifyUserBirthday(){
+
+}
+
+//get user gender from the form
+function getUserGender(gender){
+
+}
+//finds the akan name that matches the day and gender
+function findUserAkanName(){
+
+}
+//prints user akan name to the DOM
+function printUserAkanName(){
+
+}
+//clears the user input form
+function clearInput(){
+
+}
