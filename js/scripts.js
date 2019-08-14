88e7999295fdc0b07fea5c9a7775976b41a28a2e
atembamanu <atembaemm07@gmail.com>
2019-08-14 14:13:43 +0300

    initialize an array objects

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var akanObjects = [
+    {
+        Sunday:"Kwasi",
+        Monday:"Kwadwo",
+        Tuesday:"Kwabena",
+        Wednesday:"Kwaku",
+        Thurday:"Yaw",
+        Friday:"Kofi",
+        Saturday:"Kwame"
+    }, 
+    {
+        Sunday:"Akosua",
+        Sunday:"Adwoa",
+        Monday:"Abenaa",
+        Tuesday:"Kwabena",
+        Wednesday:"Akua",
+        Thurday:"Yaa",
+        Friday:"Afua",
+        Saturday:"Ama"
+    }
+]
