188174a370b4ea8a317883f8e33b19f16f71eb4a
atembamanu <atembaemm07@gmail.com>
2019-08-16 17:35:49 +0300

    add a function validate the user inputs; gender, day, month, year

--- js/scripts.js
+++ js/scripts.js
@@ -31,3 +31,99 @@ function changeBackgroundColor() {
 
 }
 setInterval(changeBackgroundColor, 2000);
+
+
+function validateForm() {
+    var gender = document.getElementsByName("gender");
+    var mdate = document.getElementById("mdate");
+    var mmonth = document.getElementById("mmonth");
+    var myear = document.getElementById("myear");
+    var formValid = false;
+    var i = 0;
+    document.getElementById("result").innerHTML = "";
+    if (mdate.value == "" || mdate.value == null) {
+
+        document.getElementById("result").innerHTML += "Day is Required";
+        document.getElementById("result").style.color = 'red';
+
+        mdate.style.border = "2px solid red";
+        return false;
+    } else {
+        mdate.style.border = "";
+        if (!isNaN(mdate.value)) {
+            if (mdate.value <= 0 || mdate.value > 31) {
+                document.getElementById("result").innerHTML += "Invalid Day";
+                document.getElementById("result").style.color = 'red';
+
+                mdate.style.border = "2px solid red";
+                return false;
+            }
+        } else {
+            document.getElementById("result").innerHTML += "Day must be a number";
+            document.getElementById("result").style.color = 'red';
+
+            mdate.style.border = "2px solid red";
+            return false;
+        }
+    }
+    if (mmonth.value == "" || mmonth.value == null) {
+        document.getElementById("result").innerHTML += "Month is Required";
+        document.getElementById("result").style.color = 'red';
+        mmonth.style.border = "2px solid red";
+        return false;
+    } else {
+        mmonth.style.border = "";
+        if (!isNaN(mmonth.value)) {
+            if (mmonth.value <= 0 || mmonth.value > 12) {
+                document.getElementById("result").innerHTML += "Invalid Month";
+                document.getElementById("result").style.color = 'red';
+
+                mmonth.style.border = "2px solid red";
+                return false;
+            }
+        } else {
+            document.getElementById("result").innerHTML += "Month must be a number";
+            document.getElementById("result").style.color = 'red';
+
+            mmonth.style.border = "2px solid red";
+            return false;
+        }
+    }
+    if (myear.value == "" || myear.value == null) {
+        document.getElementById("result").innerHTML += "Year is Required";
+        document.getElementById("result").style.color = 'red';
+        myear.style.border = "2px solid red";
+        return false;
+    } else {
+        myear.style.border = "";
+        if (!isNaN(myear.value)) {
+            if (myear.value.length != 4) {
+                document.getElementById("result").innerHTML += "Invalid Year";
+                document.getElementById("result").style.color = 'red';
+
+                myear.style.border = "2px solid red";
+                return false;
+            }
+        } else {
+            document.getElementById("result").innerHTML += "Year must be a number";
+            document.getElementById("result").style.color = 'red';
+
+            myear.style.border = "2px solid red";
+            return false;
+        }
+    }
+
+    while (!formValid && i < gender.length) {
+        if (gender[i].checked)
+            formValid = true;
+        i++;
+    }
+    if (!formValid) {
+        document.getElementById("the-gender").style.color = 'red';
+        return false;
+    }
+
+
+    return formValid;
+
+}
